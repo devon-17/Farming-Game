6b15aecee0953683c93220bcdada5023a328939b
Devon Dunham <itisdevondunham@gmail.com>
2022-04-21 10:03:44 -0400

    ongoing: ice farm

--- FarmingGame/Assets/Scripts/StoreManager.cs
+++ FarmingGame/Assets/Scripts/StoreManager.cs
@@ -1,26 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StoreManager : MonoBehaviour
 {
     public GameObject plantItem;
+
     List<Plant> plantObjects = new List<Plant>();
 
     void Awake()
     {
-        var loadPlants = Resources.LoadAll("Plants", typeof(Plant));
+        var loadPlants =
+            Resources.LoadAll("Normal Farm Plants", typeof (Plant));
         foreach (var plant in loadPlants)
         {
-            plantObjects.Add((Plant)plant);
+            plantObjects.Add((Plant) plant);
         }
-        plantObjects.Sort(SortByPrice);
+        plantObjects.Sort (SortByPrice);
 
         foreach (var plant in plantObjects)
         {
-            PlantItem newPlant = Instantiate(plantItem, transform).GetComponent<PlantItem>();
+            PlantItem newPlant =
+                Instantiate(plantItem, transform).GetComponent<PlantItem>();
             newPlant.plant = plant;
         }
+
+        // if (
+        //     SceneManager.GetActiveScene() ==
+        //     SceneManager.GetSceneByName("Farm Two")
+        // )
+        // {
+        //     var loadIcePlants =
+        //         Resources.LoadAll("Ice Farm Plants", typeof (Plant));
+        //     foreach (var plant in loadIcePlants)
+        //     {
+        //         plantObjects.Add((Plant) plant);
+        //     }
+        //     plantObjects.Sort (SortByPrice);
+
+        //     foreach (var plant in plantObjects)
+        //     {
+        //         PlantItem newPlant =
+        //             Instantiate(plantItem, transform).GetComponent<PlantItem>();
+        //         newPlant.plant = plant;
+        //     }
+        // }
     }
 
     int SortByPrice(Plant plantObjectOne, Plant plantObjectTwo)
